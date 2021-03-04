d81556fada5bfbfb067132d38e3da5624347a720
Nikolas Laus <bchik3@gmail.com>
2021-03-04 16:56:59 +0200

    Enforce HTTPS.

--- src/App.vue
+++ src/App.vue
@@ -62,7 +62,7 @@ export default {
     }
   },
   mounted() {
-    axios.get('http://bredbrains.tech/api/consultations')
+    axios.get('https://bredbrains.tech/api/consultations')
         .then(response => {
           let json = response.data
           this.consultations = json.data
